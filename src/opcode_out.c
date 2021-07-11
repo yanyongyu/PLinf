f473bf6fce0a8185f768184e43800e698b74fa92
yanyongyu <yanyongyu_1@126.com>
2021-07-12 01:26:23 +0800

    :sparkles: finish declare opcode gen

--- src/opcode_out.c
+++ src/opcode_out.c
@@ -43,6 +43,9 @@ char *format_arg(ARG arg) {
     case at_str:
       sprintf(result, "str:%s", arg.str);
       break;
+    case at_type:
+      sprintf(result, "type:%s", arg.str);
+      break;
   }
   return result;
 }
@@ -56,6 +59,35 @@ void output(FILE *fp, int index, OPCODE opcode, char *arg) {
  *                       NODES                      *
  *                                                  *
  ****************************************************/
+
+void opout_type(FILE *fp, TYPE *type) {
+  ARG temp;
+  switch (type->type) {
+    case tt_int:
+      temp = (ARG){.type = at_type, .str = "int"};
+      output(fp, next_index(), opcode_load_const, format_arg(temp));
+      break;
+    case tt_real:
+      temp = (ARG){.type = at_type, .str = "real"};
+      output(fp, next_index(), opcode_load_const, format_arg(temp));
+      break;
+    case tt_bool:
+      temp = (ARG){.type = at_type, .str = "bool"};
+      output(fp, next_index(), opcode_load_const, format_arg(temp));
+      break;
+    case tt_identifier:
+      temp = (ARG){.type = at_str, .str = type->identifier->name};
+      output(fp, next_index(), opcode_load_const, format_arg(temp));
+      break;
+    case tt_array:
+      opout_type(fp, type->array->sub_type);
+      temp = (ARG){.type = at_int, .num = type->array->array_end};
+      output(fp, next_index(), opcode_load_const, format_arg(temp));
+      temp = (ARG){.type = at_int, .num = type->array->array_start};
+      output(fp, next_index(), opcode_load_const, format_arg(temp));
+      output(fp, next_index(), opcode_build_array_type, (char *)NULL);
+  }
+}
 
 void opout_const_declare(FILE *fp, CONST_DECLARE *const_declare) {
   ARG temp;
@@ -75,6 +107,71 @@ void opout_const_declare(FILE *fp, CONST_DECLARE *const_declare) {
   output(fp, next_index(), opcode_const_declare, format_arg(temp));
 }
 
+void opout_type_declare(FILE *fp, TYPE_DECLARE *type_declare) {
+  ARG temp;
+  opout_type(fp, type_declare->type);
+  temp = (ARG){.type = at_str, .str = type_declare->id->name};
+  output(fp, next_index(), opcode_type_declare, format_arg(temp));
+}
+
+void opout_var_declare(FILE *fp, VAR_DECLARE *var_declare) {
+  ARG temp;
+  opout_type(fp, var_declare->type);
+  temp = (ARG){.type = at_str, .str = var_declare->id->name};
+  output(fp, next_index(), opcode_var_declare, format_arg(temp));
+}
+
+void opout_procedure_declare(FILE *fp, PROCEDURE_DECLARE *procedure_declare) {
+  ARG temp;
+  output(fp, next_index(), opcode_block_start, (char *)NULL);
+  NODE *node = procedure_declare->procedure->node_list;
+  while (node != NULL) {
+    opout_node(fp, node);
+    node = node->next;
+  }
+  output(fp, next_index(), opcode_block_end, (char *)NULL);
+
+  int param_count = 0;
+  PARAM_LIST *param = procedure_declare->procedure->param_list;
+  while (param != NULL) {
+    opout_type(fp, param->type);
+    temp = (ARG){.type = at_str, .str = param->id->name};
+    output(fp, next_index(), opcode_param_declare, format_arg(temp));
+    param_count++;
+    param = param->next;
+  }
+  temp = (ARG){.type = at_str, .str = procedure_declare->id->name};
+  output(fp, next_index(), opcode_load_const, format_arg(temp));
+  temp = (ARG){.type = at_int, .num = param_count};
+  output(fp, next_index(), opcode_procedure_declare, format_arg(temp));
+}
+
+void opout_function_declare(FILE *fp, FUNCTION_DECLARE *function_declare) {
+  ARG temp;
+  output(fp, next_index(), opcode_block_start, (char *)NULL);
+  NODE *node = function_declare->function->node_list;
+  while (node != NULL) {
+    opout_node(fp, node);
+    node = node->next;
+  }
+  output(fp, next_index(), opcode_block_end, (char *)NULL);
+
+  int param_count = 0;
+  PARAM_LIST *param = function_declare->function->param_list;
+  while (param != NULL) {
+    opout_type(fp, param->type);
+    temp = (ARG){.type = at_str, .str = param->id->name};
+    output(fp, next_index(), opcode_param_declare, format_arg(temp));
+    param_count++;
+    param = param->next;
+  }
+  opout_type(fp, function_declare->function->return_type);
+  temp = (ARG){.type = at_str, .str = function_declare->id->name};
+  output(fp, next_index(), opcode_load_const, format_arg(temp));
+  temp = (ARG){.type = at_int, .num = param_count};
+  output(fp, next_index(), opcode_function_declare, format_arg(temp));
+}
+
 void opout_node(FILE *fp, NODE *node) {
   if (node == NULL) return;
   switch (node->op) {
@@ -83,6 +180,18 @@ void opout_node(FILE *fp, NODE *node) {
       break;
     case op_const_declare:
       opout_const_declare(fp, node->const_declare);
+      break;
+    case op_type_declare:
+      opout_type_declare(fp, node->type_declare);
+      break;
+    case op_var_declare:
+      opout_var_declare(fp, node->var_declare);
+      break;
+    case op_procedure_declare:
+      opout_procedure_declare(fp, node->procedure_declare);
+      break;
+    case op_function_declare:
+      opout_function_declare(fp, node->function_declare);
       break;
     default:
       break;
--- src/opcode_out.h
+++ src/opcode_out.h
@@ -6,9 +6,58 @@
 #include "intermediate.h"
 
 typedef enum {
+  /* do nothing */
   opcode_nop,
+  /* declare a const variable
+   * arg: const name
+   * tos: const value
+   */
   opcode_const_declare,
+  /* declare a type variable
+   * arg: type name
+   * tos: type
+   */
+  opcode_type_declare,
+  /* declare a variable with type
+   * arg: var name
+   * tos: var type
+   */
+  opcode_var_declare,
+  /* declare a procedure
+   * arg: param count n
+   * tos: procedure name
+   * tos1~n: params
+   * tosn+1: block object
+   */
+  opcode_procedure_declare,
+  /* declare a function
+   * arg: param count n
+   * tos: function name
+   * tos1: function return type
+   * tos2~n+1: params
+   * tosn+2: block object
+   */
+  opcode_function_declare,
+  /* declare a procedure or function param
+   * push param object back onto stack
+   * arg: param name
+   * tos: param type
+   */
+  opcode_param_declare,
+  /* build array type and push type onto stack
+   * tos: array start index
+   * tos1: array end index
+   * tos2: array arg type
+   */
+  opcode_build_array_type,
+  /* push const value onto stack */
   opcode_load_const,
+  /* consume that the following opcodes all in one block
+   * do not run opcodes in block
+   */
+  opcode_block_start,
+  /* pop all opcodes in block and push a block object onto stack */
+  opcode_block_end,
 } OPCODE;
 
 extern int global_index;
@@ -17,20 +66,25 @@ void clear_index(void);
 int current_index(void);
 int next_index(void);
 
-typedef enum { at_int, at_real, at_bool, at_str } ARG_TYPE;
+typedef enum { at_int, at_real, at_bool, at_str, at_type } ARG_TYPE;
 
 typedef struct {
   ARG_TYPE type;
   union {
-    long num;
-    double real_num;
-    const char *str;
+    long num;         // int, bool
+    double real_num;  // real
+    const char *str;  // string, type
   };
 } ARG;
 
 char *format_arg(ARG arg);
 void output(FILE *fp, int index, OPCODE opcode, char *arg);
+void opout_type(FILE *fp, TYPE *type);
 void opout_const_declare(FILE *fp, CONST_DECLARE *const_declare);
+void opout_type_declare(FILE *fp, TYPE_DECLARE *type_declare);
+void opout_var_declare(FILE *fp, VAR_DECLARE *var_declare);
+void opout_procedure_declare(FILE *fp, PROCEDURE_DECLARE *procedure_declare);
+void opout_function_declare(FILE *fp, FUNCTION_DECLARE *function_declare);
 void opout_node(FILE *fp, NODE *node);
 
 #endif
